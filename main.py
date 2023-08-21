db5c4820506832c40b85e5215be93d57875887ed
thomasservin <thomas_servin1@my.vcccd.edu>
2023-08-20 20:34:58 -0700

    create main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     # Comlete your code here
     ##################################################
     original_str = 'Python Programming'
-    index_slicing = 5 
+    index_slicing = 5  
     sub1 = original_str[:index_slicing]
     sub2 = original_str[index_slicing:]
     merge_str = sub2 + sub1
