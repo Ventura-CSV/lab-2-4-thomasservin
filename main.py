5fb3ee9386da85a0a1a33efb81777c23ee86fca2
thomasservin <thomas_servin1@my.vcccd.edu>
2023-08-22 21:19:05 -0700

    create main.py

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ def main():
     # Comlete your code here
     ##################################################
     original_str = 'Python Programming'
-    index_slicing = 5  
-    sub1 = original_str[5:index_slicing]
-    sub2 = original_str[index_slicing:5]
+
+    index_slicing = 5
+
+    sub1 = original_str[index_slicing:]
+    sub2 = original_str[:index_slicing]
     merge_str = sub2 + sub1
-    
+
     print(sub2)
     print(sub1)
-    print(merge_str) 
+    print(merge_str)
+  
+  
    
     pass
     if __name__ == '__main__':
         main()
+    
